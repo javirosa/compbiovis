82c59984ae7b2d6cf293ec6af2e76d1b3ca32f92
nicholas.kong <nicholas.kong@7139a3a0-c1c9-11de-a0e9-83d03c287164>
2009-12-12 23:27:43 +0000

    Added coloring by organism.

--- vieprot/ProteinInteractionViewer/src/vieprot/lib/InterfaceConstants.java
+++ vieprot/ProteinInteractionViewer/src/vieprot/lib/InterfaceConstants.java
@@ -1,8 +1,14 @@
 package vieprot.lib;
 
+import prefuse.util.ColorLib;
+
 public class InterfaceConstants {
 
 	public static String SORT_OPTIONS_ID = "id";
 	public static String SORT_OPTIONS_NUM_NODES = "# proteins";
 	
+	// Colors
+	public static int MODULE_O_COLOR = ColorLib.rgb(255, 121, 121);
+	public static int MODULE_1_COLOR = ColorLib.rgb(121, 174, 255);
+
 }
